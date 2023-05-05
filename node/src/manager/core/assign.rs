cf909294b48ad7b8a9ce3866a38b56374dbb9a2c
YassinEldeeb <yassineldeeb94@gmail.com>
2023-05-05 13:46:01 +0300

    refactor: extract some of the core functions

--- node/src/manager/core/assign.rs
+++ node/src/manager/core/assign.rs
@@ -7,7 +7,7 @@ use std::time::Duration;
 
 use crate::manager::deployment::DeploymentSearch;
 
-pub async fn unassign(
+pub fn unassign(
     primary: ConnectionPool,
     sender: &NotificationSender,
     search: &DeploymentSearch,
@@ -21,7 +21,6 @@ pub async fn unassign(
         .locate_site(locator.clone())?
         .ok_or_else(|| anyhow!("failed to locate site for {locator}"))?;
 
-    println!("unassigning {locator}");
     let changes = conn.unassign_subgraph(&site)?;
     conn.send_store_event(sender, &StoreEvent::new(changes))?;
 
@@ -33,7 +32,7 @@ pub fn reassign(
     sender: &NotificationSender,
     search: &DeploymentSearch,
     node: String,
-) -> Result<(), Error> {
+) -> Result<Option<NodeId>, Error> {
     let node = NodeId::new(node.clone()).map_err(|()| anyhow!("illegal node id `{}`", node))?;
     let locator = search.locate_unique(&primary)?;
 
@@ -46,30 +45,17 @@ pub fn reassign(
     let changes = match conn.assigned_node(&site)? {
         Some(cur) => {
             if cur == node {
-                println!("deployment {locator} is already assigned to {cur}");
                 vec![]
             } else {
-                println!("reassigning {locator} to {node} (was {cur})");
                 conn.reassign_subgraph(&site, &node)?
             }
         }
-        None => {
-            println!("assigning {locator} to {node}");
-            conn.assign_subgraph(&site, &node)?
-        }
+        None => conn.assign_subgraph(&site, &node)?,
     };
     conn.send_store_event(sender, &StoreEvent::new(changes))?;
 
-    // It's easy to make a typo in the name of the node; if this operation
-    // assigns to a node that wasn't used before, warn the user that they
-    // might have mistyped the node name
-    let mirror = catalog::Mirror::primary_only(primary);
-    let count = mirror.assignments(&node)?.len();
-    if count == 1 {
-        println!("warning: this is the only deployment assigned to {node}");
-        println!("         are you sure it is spelled correctly?");
-    }
-    Ok(())
+    // Return the assigned node for further processing
+    Ok(conn.assigned_node(&site)?)
 }
 
 pub fn pause_or_resume(
